1051caa56ac146b5ec0bfda2743c05a30dda10d3
Farrah Bousetta <farrah.bousetta@gmail.com>
2017-08-22 15:33:41 -0700

    minor edits to spacing

--- solution/sync-sorted-merge.js
+++ solution/sync-sorted-merge.js
@@ -5,8 +5,7 @@ const MinHeap = require('heap-min-max').MinHeap;
 
 module.exports = (logSources, printer) => {
     let heap = new MinHeap();
-    //MinHeap { items: [], _isMinHeap: true }
-    //iterate through all log sources
+
     logSources.forEach((logSource) => {
         let logEntry = logSource.pop()
         if (logEntry) {
@@ -27,36 +26,3 @@ module.exports = (logSources, printer) => {
     }
     printer.done()
 }
-
-
-
-// function loadHeap (source, heap) {
-//     const entry = source.pop()
-//     if (entry.date !== undefined) {
-//         heap.push(entry.date, {source, entry})
-//     }
-// }
-
-
-// module.exports = (logSources, printer) => {
-// 	//instantiate new minimimum heap
-//     const heap = new MinHeap()
-//     //MinHeap { items: [], _isMinHeap: true }
-//     //iterate through all log sources
-//     logSources.forEach( source => {
-//     	//for each source
-//         loadHeap(source, heap)
-//         console.log(heap)
-//      /*MinHeap {
-//   items: [ [ 2017-07-04T09:13:06.962Z, [Object] ] ],
-//   _isMinHeap: true } */
-//     })
-//     let nextEntry = heap.pop()
-//     while (nextEntry !== undefined) {
-//         const { source, entry } = nextEntry[1]
-//         printer.print(entry)
-//         loadHeap(source, heap)
-//         nextEntry = heap.pop()
-//     }
-//     printer.done()
-// }
