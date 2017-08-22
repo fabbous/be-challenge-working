7bfe45fed478cc38d67060dd10a3c271a73dacee
Farrah Bousetta <farrah.bousetta@gmail.com>
2017-08-22 15:06:18 -0700

    solutionfor sync sorted merge

--- solution/sync-sorted-merge.js
+++ solution/sync-sorted-merge.js
@@ -3,32 +3,60 @@
 const MinHeap = require('heap-min-max').MinHeap;
 
 
-
-function loadHeap (source, heap) {
-    const entry = source.pop()
-    entry && heap.push(entry.date, {source, entry})
-}
-
-
 module.exports = (logSources, printer) => {
-	//instantiate new minimimum heap
-    const heap = new MinHeap()
+    let heap = new MinHeap();
     //MinHeap { items: [], _isMinHeap: true }
     //iterate through all log sources
-    logSources.forEach( source => {
-    	//for each source
-        loadHeap(source, heap)
-        console.log(heap)
-     /*MinHeap {
-  items: [ [ 2017-07-04T09:13:06.962Z, [Object] ] ],
-  _isMinHeap: true } */
-    })
-    let nextEntry = heap.pop()
-    while (nextEntry !== undefined) {
-        const { source, entry } = nextEntry[1]
-        printer.print(entry)
-        loadHeap(source, heap)
-        nextEntry = heap.pop()
+    logSources.forEach((logSource) => {
+        let logEntry = logSource.pop()
+        if (logEntry) {
+            heap.push(logEntry.date, {logSource, logEntry})
+        }
+    });
+
+    let nextLogEntry = heap.pop()
+    while (nextLogEntry) {
+        let logSource = nextLogEntry[1].logSource
+        let logEntry = nextLogEntry[1].logEntry
+        printer.print(logEntry)
+        logEntry = logSource.pop();
+        if (logEntry) {
+            heap.push(logEntry.date, {logSource, logEntry})
+        }
+        nextLogEntry = heap.pop()
     }
     printer.done()
 }
+
+
+
+// function loadHeap (source, heap) {
+//     const entry = source.pop()
+//     if (entry.date !== undefined) {
+//         heap.push(entry.date, {source, entry})
+//     }
+// }
+
+
+// module.exports = (logSources, printer) => {
+// 	//instantiate new minimimum heap
+//     const heap = new MinHeap()
+//     //MinHeap { items: [], _isMinHeap: true }
+//     //iterate through all log sources
+//     logSources.forEach( source => {
+//     	//for each source
+//         loadHeap(source, heap)
+//         console.log(heap)
+//      /*MinHeap {
+//   items: [ [ 2017-07-04T09:13:06.962Z, [Object] ] ],
+//   _isMinHeap: true } */
+//     })
+//     let nextEntry = heap.pop()
+//     while (nextEntry !== undefined) {
+//         const { source, entry } = nextEntry[1]
+//         printer.print(entry)
+//         loadHeap(source, heap)
+//         nextEntry = heap.pop()
+//     }
+//     printer.done()
+// }
