e899e98d9c1050e2219527de88a41631400f3292
Farrah Bousetta <farrah.bousetta@gmail.com>
2017-08-22 15:32:51 -0700

    solution for async sorted merge

--- solution/async-sorted-merge.js
+++ solution/async-sorted-merge.js
@@ -1,23 +1,44 @@
 'use strict'
-const MinHeap = require('min-heap')
-// module.exports = (logSources, printer) => {
-// 	throw new Error('Not implemented yet!  That part is up to you!')
-// }
 
-async function loadHeap (source, heap) {
-    const entry = await source.pop()
-    entry && heap.push(entry.date, {source, entry})
-}
+const MinHeap = require('heap-min-max').MinHeap;
+const Promises = require('bluebird')
+
+
 
 module.exports = async (logSources, printer) => {
-    const heap = new MinHeap()
-    await Promise.all(logSources.map(source => loadHeap(source, heap)))
-    let nextEntry = heap.pop()
-    while (nextEntry !== undefined) {
-        const { source, entry } = nextEntry[1]
-        printer.print(entry)
-        await loadHeap(source, heap)
-        nextEntry = heap.pop()
+    let heap = new MinHeap();
+
+    let logPromises = [];
+    logSources.forEach((logSource) => {
+        let logEntry = logSource.pop()
+        logPromises.push(logSource.popAsync().then((logEntry => {
+        if (logEntry) {
+            heap.push(logEntry.date, {logSource, logEntry})
+        }
+      })));
+    });
+
+   Promises.all(logPromises).then(() => {
+    printAllLogEntries().then(() => {
+        printer.done();
+    });
+   });
+
+
+  function printAllLogEntries() {
+    let nextLogEntry = heap.pop()
+    while (nextLogEntry) {
+        let logSource = nextLogEntry[1].logSource
+        let logEntry = nextLogEntry[1].logEntry
+        printer.print(logEntry)
+        logEntry = logSource.pop();
+        if (logEntry) {
+            heap.push(logEntry.date, {logSource, logEntry})
+        }
+        nextLogEntry = heap.pop()
     }
     printer.done()
+  }
 }
+
+   
